1bd0093084069ee3e39e2037bfcc6ab54070d7c6
Karol Pudło <106621@uksw.edu.pl>
2019-10-26 12:24:45 +0200

    KAROL PUDLO COMMIT

---
+++ test/src/test/test.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class test {
+	String str = "sdad";
+	Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
