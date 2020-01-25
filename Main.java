fff0b977b06d99c1fbdbd1ca907d5d7aea69c92f
Construct97 <50079472+Construct97@users.noreply.github.com>
2020-01-25 22:08:37 +0100

    7.12_4

--- Main.java
+++ Main.java
@@ -1,24 +1,50 @@
-public class Main<T> {
+package Zadanie4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+public class Main {
     public Main() {
     }
 
-    public boolean contains(T[] Kolekcja, T Generalnie_ele) {
-        int var3 = Kolekcja.length;
-        byte var6 = 0;
-        if (var6 < var3) {
-            T item = Kolekcja[var6];
-            return item.equals(Generalnie_ele);
-        } else {
-            return false;
-        }
-    }
-
     public static void main(String[] args) {
-        String[] array = new String[]{"element", "item"};
-        String element1 = "element";
-        String element2 = "element2";
-        Main main = new Main();
-        System.out.println(main.contains(array, element1));
-        System.out.println(main.contains(array, element2));
+        People people1 = new People(10, "Zbigniew", "Stonoga");
+        People people2 = new People(20, "Tommy", "Ferrari");
+        People people3 = new People(30, "Daniel", "Zieliński");
+        List<People> peopleList = new ArrayList();
+        peopleList.add(people1);
+        peopleList.add(people2);
+        peopleList.add(people3);
+        Iterator var = peopleList.iterator();
+
+        People temp;
+        while(var.hasNext()) {
+            temp = (People)var.next();
+            System.out.println(temp.PrintInfo());
+        }
+
+        peopleList.sort(Comparator.comparingInt((a) -> {
+            return a.wiek;
+        }));
+        Collections.reverse(peopleList);
+        var = peopleList.iterator();
+
+        while(var.hasNext()) {
+            temp = (People)var.next();
+            System.out.println(temp.PrintInfo());
+        }
+
+        peopleList.sort(Comparator.comparing((a) -> {
+            return a.nazwisko;
+        }));
+        Collections.reverse(peopleList);
+        var = peopleList.iterator();
+
+        while(var.hasNext()) {
+            temp = (People)var.next();
+            System.out.println(temp.PrintInfo());
+        }
+
     }
 }
